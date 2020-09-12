748f7f89546023e194a6bd0478080e6febcbcf1e
Maha Khudhair <mahaalsarraj85@gmail.com>
2020-09-12 22:00:00 +0300

    Updates

---
+++ precache-manifest.2a14da5edc8682aa3f9919dca759f1b1.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c7208889725a221662d06cdca508e9c",
+    "url": "/calculator-app/index.html"
+  },
+  {
+    "revision": "1a21acff303ac4837264",
+    "url": "/calculator-app/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cc70b90967d98b7c668c",
+    "url": "/calculator-app/static/js/2.9d006f93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/calculator-app/static/js/2.9d006f93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a21acff303ac4837264",
+    "url": "/calculator-app/static/js/main.f98a7933.chunk.js"
+  },
+  {
+    "revision": "3f465b5e0c78badb6f74",
+    "url": "/calculator-app/static/js/runtime-main.d16fea3e.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/calculator-app/static/media/logo.5d5d9eef.svg"
+  }
+]);
